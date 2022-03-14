d5ca225c0df0fc3438ca81d4718b72c2234d9fe9
crishueso29 <cristianleonardorueda@hotmail.com>
2022-03-14 17:18:08 -0500

    modulo de historia clinica casi terminado

--- server/controllers/historia.controller.js
+++ server/controllers/historia.controller.js
@@ -35,6 +35,81 @@ const crearHistoria = async(req=request, res=response) =>{
 
 }
 
+/**
+ * Método encargado de obtener una historia en específica
+ * @param {*} req 
+ * @param {*} res 
+ */
+const getHistoria = async(req=request, res=response)=>{
+
+    try {
+
+        const {id} = req.params;
+        
+        const historia = await prisma.historia.findUnique({
+            where:{
+                mascotaId:Number(id)
+            },
+            include:{
+                mascota:true
+            }
+        });
+
+        res.status(200).json({
+            ok:true,
+            historia
+        })
+
+        
+    } catch (error) {
+        res.status(500).json({
+            ok:false,
+            msg:'Error, hable con el administrador'
+        })
+    }
+
+}
+
+
+/**
+ * Método encargado de listar todas las historias clínicas registradas en el sistema
+ * @param {*} req 
+ * @param {*} res 
+ */
+const getHistorias = async(req=request, res=response)=>{
+
+    try {
+        
+        const [numero, historias ] = await Promise.all([
+            
+            prisma.historia.count(),
+            prisma.historia.findMany({
+                include:{
+                    mascota:true
+                }
+            })
+        ])
+
+
+        res.status(200).json({
+            ok:true,
+            numero,
+            historias
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            ok:false,
+            msg:"Error, hable con el administrador"
+        
+        })
+    }
+
+}
+
+
 module.exports = {
-    crearHistoria
+    crearHistoria,
+    getHistoria,
+    getHistorias
 }
--- server/routes/historia.routes.js
+++ server/routes/historia.routes.js
@@ -1,10 +1,13 @@
 const {Router} = require('express');
-const { crearHistoria } = require('../controllers/historia.controller');
+const { crearHistoria, getHistoria,getHistorias} = require('../controllers/historia.controller');
 
 
 
 const router = Router();
 
+router.get('/:id',getHistoria)
+router.get('/',getHistorias);
 router.post('/',crearHistoria);
+
 
 module.exports= router;

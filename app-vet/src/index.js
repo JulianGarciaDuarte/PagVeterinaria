b1a6ccd1e309a39c58f3a418f90156c8d38a1bf3
Julián García Duarte <juliangarciaduarte@gmail.com>
2022-03-15 21:47:37 -0500

    _

--- app-vet/src/index.js
+++ app-vet/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import HistoriaClinica from './HistoriaClinica';
+import AnunciosBar from './AnunciosBar';
+import NavBar from './NavBar';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    
+    <AnunciosBar/>
+    <NavBar/>
+    <HistoriaClinica/>
   </React.StrictMode>,
   document.getElementById('root')
 );

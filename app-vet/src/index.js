e72b097305cbd1203777d6a21eecebf453b53622
Julián García Duarte <juliangarciaduarte@gmail.com>
2022-03-16 18:09:12 -0500

    Se agrega el otro menu y se hace responsive

--- app-vet/src/index.js
+++ app-vet/src/index.js
@@ -5,13 +5,20 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import HistoriaClinica from './HistoriaClinica';
 import AnunciosBar from './AnunciosBar';
 import NavBar from './NavBar';
+import NavBar_M from './NavBar_M';
 import Footer from './Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <AnunciosBar/>
     <NavBar/>
-    <HistoriaClinica/>
+    <NavBar_M/>
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<HistoriaClinica />}/>
+    </Routes>
+    </BrowserRouter>
     <Footer/>
   </React.StrictMode>,
   document.getElementById('root')

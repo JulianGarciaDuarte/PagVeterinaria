94b21c87a346263a95cdf3c181bc7a917d2861f7
Julián García Duarte <juliangarciaduarte@gmail.com>
2022-03-15 21:33:43 -0500

    restauracion

--- app-vet/src/App.js
+++ app-vet/src/App.js
@@ -6,12 +6,7 @@ import HistoriaClinica from './HistoriaClinica';
 function App() {
   return (
     <>
-      <AnunciosBar/>
-      <NavBar/>
-      <div className="main-container">
-      <ProfileDog/>
-      <HistoriaClinica/>
-      </div>
+      <p>hola</p>
     </>
   )
 }
